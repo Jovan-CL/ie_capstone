a2b61c1f1d19e9ffb9c0c5b7772a651dda1637d2
Jovan <jovanlaguitaoWORK@gmail.com>
2024-05-06 20:39:02 +0800

    Paunti unti, Nasa login page ako nagawa

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,24 +1,46 @@
 const Login = () => {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <a href="http://">
-              <img src="./assets/piie-logo-cropped.png" alt="PIIE logo" />
-            </a>
-          </li>
-          <li>
-            <img src="./assets/ie-connect-new.png" alt="" />
-          </li>
-          <li>
-            <a href="https://www.pup.edu.ph/">
-              <img src="./assets/pup-logo.png" alt="" />
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <a href="http://">
+                <img src="./assets/piie-logo-cropped.png" alt="PIIE logo" />
+              </a>
+            </li>
+            <li>
+              <img src="./assets/ie-connect-new.png" alt="" />
+            </li>
+            <li>
+              <a href="https://www.pup.edu.ph/">
+                <img src="./assets/pup-logo.png" alt="" />
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <section>
+          <div>
+            <h1>Connect, Network, and Inspire</h1>
+          </div>
+          <div>
+            <form action="">
+              <input type="text" />
+              <input type="text" />
+              <p>Forgot Password?</p>
+              <button type="submit">Sign in</button>
+            </form>
+          </div>
+          <footer>
+            <div>
+              <p>admin</p>
+            </div>
+          </footer>
+        </section>
+      </main>
+    </>
   );
 };
 
